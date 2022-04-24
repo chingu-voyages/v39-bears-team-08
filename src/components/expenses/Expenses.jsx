7a39f5232c395ae38593e1c5e6481962d395c20a
Luis Castillo <mr.luiscastillo@gmail.com>
2022-04-24 19:11:45 -0400

    add value to start at 0

--- src/components/expenses/Expenses.jsx
+++ src/components/expenses/Expenses.jsx
@@ -1,4 +1,4 @@
-import React, {useState } from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col, Form, Button, DropdownButton, Dropdown } from 'react-bootstrap';
 
 
@@ -31,43 +31,43 @@ const Expenses = () => {
                         <Form>
                             <Form.Group className="mb-3" controlId="groceries">
                                 <Form.Label>Groceries</Form.Label>
-                                <Form.Control type="number" placeholder="Groceries" />
+                                <Form.Control type="number" placeholder="Groceries" value={inputsValue.groceries} />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="restaurant">
                                 <Form.Label>Restaurant</Form.Label>
-                                <Form.Control type="number" placeholder="Restaurant" />
+                                <Form.Control type="number" placeholder="Restaurant" value={inputsValue.restaurant} />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="bar-cafe">
                                 <Form.Label>Bar/Cafe</Form.Label>
-                                <Form.Control type="number" placeholder="Bar/Cafe" />
+                                <Form.Control type="number" placeholder="Bar/Cafe" value={inputsValue.barcafe}/>
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="rent">
                                 <Form.Label>Rent</Form.Label>
-                                <Form.Control type="number" placeholder="Rent" />
+                                <Form.Control type="number" placeholder="Rent" value={inputsValue.rent} />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="utilities">
                                 <Form.Label>Utilities</Form.Label>
-                                <Form.Control type="number" placeholder="Utilities" />
+                                <Form.Control type="number" placeholder="Utilities" value={inputsValue.utilities} />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="insurance">
                                 <Form.Label>Insurance</Form.Label>
-                                <Form.Control type="number" placeholder="Insurance" />
+                                <Form.Control type="number" placeholder="Insurance" value={inputsValue.insurance} />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="fuel">
                                 <Form.Label>Fuel</Form.Label>
-                                <Form.Control type="number" placeholder="Fuel" />
+                                <Form.Control type="number" placeholder="Fuel" value={inputsValue.fuel} />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="entertaiment">
                                 <Form.Label>Entertaiment</Form.Label>
-                                <Form.Control type="number" placeholder="Entertaiment" />
+                                <Form.Control type="number" placeholder="Entertaiment" value={inputsValue.entertaiment} />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="communication">
                                 <Form.Label>Communication</Form.Label>
-                                <Form.Control type="number" placeholder="Communication" />
+                                <Form.Control type="number" placeholder="Communication" value={inputsValue.communication} />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="total">
                                 <Form.Label>Total</Form.Label>
-                                <Form.Control type="number" placeholder="Total" />
+                                <Form.Control type="number" placeholder="Total" value={inputsValue.total} />
                             </Form.Group>
 
                             <Button variant="primary" type="submit">Save</Button>
