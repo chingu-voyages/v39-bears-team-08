d7e3e0d65478d1dc8451bde7596163f38775801c
Luis Castillo <mr.luiscastillo@gmail.com>
2022-04-24 21:31:06 -0400

    add expenses route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import Expenses from "./components/expenses/Expenses";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import BudgetPage from "./pages/budgetPage/BudgetPage";
+import Expenses from "./components/expenses/Expenses";
 
 
 function App() {
   return (
     <div className="App">
-      <Expenses />
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/budgetpage" element={<BudgetPage />}></Route>
+        <Route path="/expenses" element={<Expenses />}></Route>
       </Routes>
     </div>
   );
