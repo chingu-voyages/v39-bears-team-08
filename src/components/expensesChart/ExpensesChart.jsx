153e793d14973e9d0772875a52a71bb73fe78f66
Luis Castillo <mr.luiscastillo@gmail.com>
2022-04-30 19:51:26 -0400

    add background color and border color

--- src/components/expensesChart/ExpensesChart.jsx
+++ src/components/expensesChart/ExpensesChart.jsx
@@ -8,6 +8,35 @@ const ExpensesChart = () =>{
             <Doughnut 
                 data={{
                     labels: ['Groceries', 'Restaurant', 'BarCafe', 'Rent', 'Utilities', 'Insurances', 'Fuel', 'Entertainment', 'Communication'],
+                    datasets: [
+                        {
+                          label: '# of Expenses',
+                          data: [12, 19, 3, 5, 2, 3, 8, 14, 20],
+                          backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(128, 0, 128, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(199, 21, 133, 0.2)',
+                            'rgba(188, 143, 143, 0.2)',
+                          ],
+                          borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(128, 0, 128, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(199, 21, 133, 1)',
+                            'rgba(188, 143, 143, 1)',
+                          ],
+                          borderWidth: 0.5,
+                        },
+                      ],
                 }}
             />
         </div>
